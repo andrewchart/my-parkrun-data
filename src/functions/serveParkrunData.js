dd0db0b157f1f46630e4a8b3f11a01834e3d9fcd
Andrew Chart <achart@hotmail.com>
2026-08-30 00:05:22 +0100

    Adds a dummy endpoint

---
+++ src/functions/serveParkrunData.js
@@ -0,0 +1,69 @@
+const { app } = require('@azure/functions');
+
+app.http('example', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        let parkruns = [
+
+            new Parkrun(
+                "Woking", 
+                new Date(2026,8,5), 
+                611, 
+                100, 
+                new Temporal.Duration(0,0,0,0,25,21), 
+                44.19,
+                false
+            ),
+
+            new Parkrun(
+                "Woking", 
+                new Date(2026,8,19), 
+                611, 
+                250, 
+                new Temporal.Duration(0,0,0,0,30,1), 
+                24.0,
+                false
+            ),
+
+            new Parkrun(
+                "Guildford", 
+                new Date(2027,3,24), 
+                611, 
+                175, 
+                new Temporal.Duration(0,0,0,0,26,54), 
+                60.19,
+                false
+            ),
+
+            new Parkrun(
+                "Brooklands", 
+                new Date(2025,3,21), 
+                160, 
+                40, 
+                new Temporal.Duration(0,0,0,0,22,1), 
+                55.36,
+                true
+            )
+        ];
+
+        let response = {
+            body: JSON.stringify({ message: 'OK', parkruns }),
+            status: 200
+        }
+
+        return response;
+    }
+});
+
+function Parkrun(event, run_date, run_number, pos, time, age_grade, pb) {
+    this.event = event; 
+    this.run_date = run_date; 
+    this.run_number = run_number; 
+    this.pos = pos; 
+    this.time = time; 
+    this.age_grade = age_grade; 
+    this.pb = pb;
+}
