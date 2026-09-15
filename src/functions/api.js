0c48ca43085cd8043cc811e09dce11e43af46d5f
Andrew Chart <achart@hotmail.com>
2026-09-16 00:34:24 +0100

    Adds ability to filter results by date

--- src/functions/api.js
+++ src/functions/api.js
@@ -20,7 +20,10 @@ app.http('parkruns', {
 
         let parkruns = [];
         let data = await tableService.listEntities({
-            queryOptions: { select: ['event','run_date','run_number','pos','time','age_grade','pb'] }
+            queryOptions: { 
+                select: ['event','run_date','run_number','pos','time','age_grade','pb'],
+                filter: constructFilter(request.params)
+            }
         });
 
         for await (const parkrun of data) {
@@ -36,3 +39,45 @@ app.http('parkruns', {
         return response;
     }
 });
+
+
+/**
+ * Constructs a string to be used as filter condition/s when querying the data table
+ * 
+ * @param   {Array}  params All GET params supplied in the http request
+ * @returns {String}        String containing query conditions to filter the table by
+ */
+function constructFilter(params) {
+
+    const { from, to } = params;
+
+    let filterQueryParts = [];
+    
+    // Return runs from this date onwards
+    if( validateFilterParamDate(from) ) {
+        filterQueryParts.push(`run_date ge datetime'${from}T00:00:00.000'`);
+    }
+  
+    // Returns runs up to and including this date
+    if( validateFilterParamDate(to) ) {
+        filterQueryParts.push(`run_date le datetime'${to}T23:59:59.999'`);
+    }
+
+    if(filterQueryParts.length > 0) {
+        return filterQueryParts.join(' and ');
+    } else {
+        return null;
+    }
+
+}
+
+/**
+ * Validates the date parameters supplied in the API GET request
+ * 
+ * @param   {String}  dateString  String expected to represent a date from the query param
+ * @returns {Boolean}             True if the date matches the format YYYY-MM-DD
+ */
+function validateFilterParamDate(dateString) {
+    const datePattern = new RegExp("^2[0-9]{3}-[0-1][0-9]-[0-3][0-9]$");
+    return datePattern.test(dateString);
+}
